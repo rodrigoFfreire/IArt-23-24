b1d85f5ca7f1d2c0d5d20be8b02b258935a6338e
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2024-05-01 13:27:07 +0000

    reverted to using letters as storage

--- src/pipe.py
+++ src/pipe.py
@@ -21,40 +21,6 @@ from search import (
     recursive_best_first_search,
 )
 
-piece_to_code = {
-    "FC": 0,
-    "FB": 1,
-    "FE": 2,
-    "FD": 3,
-    "BC": 4,
-    "BB": 5,
-    "BE": 6,
-    "BD": 7,
-    "VC": 8,
-    "VB": 9,
-    "VE": 10,
-    "VD": 11,
-    "LH": 12,
-    "LV": 13
-}
-
-code_to_piece = (
-    "FC",
-    "FB",
-    "FE",
-    "FD",
-    "BC",
-    "BB",
-    "BE",
-    "BD",
-    "VC",
-    "VB",
-    "VE",
-    "VD",
-    "LH",
-    "LV"
-)
-
 
 class PipeManiaState:
     state_id = 0
@@ -79,22 +45,22 @@ class Board:
 
     def get_value(self, row: int, col: int) -> str:
         """Devolve o valor na respetiva posição do tabuleiro."""
-        return code_to_piece(self.storage[row * self.size + col])
+        return self.storage[row * self.size + col]
 
     def adjacent_vertical_values(self, row: int, col: int) -> (str, str):
         """Devolve os valores imediatamente acima e abaixo,
         respectivamente."""
         return (
-            code_to_piece[self.storage[(row - 1) * self.size + col]] if row > 0 else None,
-            code_to_piece[self.storage[(row + 1) * self.size + col]] if row < self.size - 1 else None
+            self.storage[(row - 1) * self.size + col] if row > 0 else None,
+            self.storage[(row + 1) * self.size + col] if row < self.size - 1 else None
         )
 
     def adjacent_horizontal_values(self, row: int, col: int) -> (str, str):
         """Devolve os valores imediatamente à esquerda e à direita,
         respectivamente."""
         return (
-            code_to_piece[self.storage[row * self.size + col - 1]] if col > 0 else None,
-            code_to_piece[self.storage[row * self.size + col + 1]] if col < self.size - 1 else None
+            self.storage[row * self.size + col - 1] if col > 0 else None,
+            self.storage[row * self.size + col + 1] if col < self.size - 1 else None
         )
 
     def print(self) -> str:
@@ -102,9 +68,9 @@ class Board:
         out = ""
 
         # Loops through storage and builds the output
-        for piece_code in self.storage.tolist():
+        for piece in self.storage.tolist():
             i += 1
-            out += code_to_piece[piece_code]
+            out += piece
             if i % self.size == 0:
                 if i == self.size ** 2:
                     return out
@@ -124,12 +90,12 @@ class Board:
         storage = np.array('\t'.join(lines).replace('\n', '').split('\t'))
 
         # Converts the visual representation of all pieces into its interal representation
-        storage = np.vectorize(Board.convert_piece)(storage)
+        # storage = np.vectorize(Board.convert_piece)(storage)
 
         return Board(int(math.sqrt(len(storage))), storage)
 
-    def convert_piece(piece: str) -> int:
-        return piece_to_code[piece]
+    # def convert_piece(piece: str) -> int:
+    #   return piece_to_code[piece]
 
     # TODO: outros metodos da classe
 
